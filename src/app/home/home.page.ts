be6c40761ff1528d150c10b1f798a97a0ed0d171
imraphavf <raphael.v.freitas03@gmail.com>
2024-10-08 19:28:54 -0300

    comecando a fazer a pagina home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // Aqui, você define os dados dos macronutrientes
+  macronutrientes = {
+    calorias: { consumidas: 1291, restantes: 826, queimadas: 244 },
+    carboidratos: { consumidos: 206, meta: 258 },
+    proteinas: { consumidos: 35, meta: 103 },
+    gorduras: { consumidos: 32, meta: 68 }
+  };
 
   constructor() {}
 
