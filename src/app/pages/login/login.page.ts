7e92fb4003a345e17629743b138cb5b04173b37b
imraphavf <raphael.v.freitas03@gmail.com>
2024-10-09 19:11:02 -0300

    fazendo pagina login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  email: string = '';  // Variável para o email
+  password: string = '';  // Variável para a senha
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
+  // Função para efetuar o login
+  login() {
+    if (this.email && this.password) {
+      console.log('Email:', this.email, 'Senha:', this.password);
+      // Aqui você pode adicionar sua lógica de autenticação
+      // Exemplo: chamar um serviço de autenticação
+    } else {
+      console.log('Por favor, preencha todos os campos.');
+    }
+  }
+
+  // Função para navegar para a página de recuperação de senha
+  forgotPassword() {
+    this.navCtrl.navigateForward('/forgot-password');
+  }
+
+  // Função para navegar para a página de registro
+  register() {
+    this.navCtrl.navigateForward('/register');
+  }
 }
