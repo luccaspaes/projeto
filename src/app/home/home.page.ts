7e3c37c88939a6de7160b971cab757e4172c4488
imraphavf <raphael.v.freitas03@gmail.com>
2024-10-14 11:05:18 -0300

    teste de algumas funcoes da api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,39 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { SpoonacularService } from '../services/spoonacular.service';
+import { SpoonacularService } from '../services/spoonacular.service'; // Importa o serviço
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
-  // Aqui, você define os dados dos macronutrientes
-  macronutrientes = {
-    calorias: { consumidas: 1291, restantes: 826, queimadas: 244 },
-    carboidratos: { consumidos: 206, meta: 258 },
-    proteinas: { consumidos: 35, meta: 103 },
-    gorduras: { consumidos: 32, meta: 68 }
-  };
-
+export class HomePage implements OnInit {
+  
   recipeData: any[] = [];
+  isFoodOptionsVisible: boolean = false; // Dados das receitas
 
   constructor(private spoonacularService: SpoonacularService) {}
 
   ngOnInit() {
-    this.getRecipes();
+    this.getRecipes(); // Chama a função para buscar receitas
   }
 
-
+  // Função para buscar receitas da API
   getRecipes() {
     this.spoonacularService.getRandomRecipes().subscribe(
       (response) => {
-        this.recipeData = response.recipes;
-        console.log('Receitas obtidas:', this.recipeData);
+        this.recipeData = response.recipes; // Armazena as receitas
+        console.log('Dados das Receitas:', this.recipeData);
+        this.isFoodOptionsVisible = true
       },
       (error) => {
-        console.error('Erro ao obter receitas:', error);
+        console.error('Erro ao buscar receitas:', error);
       }
     );
+  }
+
+  // Função para adicionar alimento
+  addFood() {
+    console.log('Adicionando alimento...');
+    this.getRecipes();
+    // Aqui você pode adicionar lógica para adicionar um alimento (pode ser uma requisição à API, por exemplo)
+  }
+
+  // Função para buscar alimento (ou outra lógica)
+  searchFood() {
+    console.log('Buscando alimento...');
+    // Aqui você pode adicionar lógica para buscar alimento, usando o serviço se necessário
   }
 }
 
